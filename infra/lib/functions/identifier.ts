2bdaab5326b70e46e49b63fd0023b28bea9db7c5
Adnan Kurt <adnankurt16@gmail.com>
2023-12-06 06:30:52 +0300

    Fix: Updated the environment variable names

--- infra/lib/functions/identifier.ts
+++ infra/lib/functions/identifier.ts
@@ -1,13 +1,13 @@
 exports.handler = async (event: any, context: any) => {
   console.log('Username:', event.username, 'ServerId: ', event.serverId);
 
-  // Conditionally allow or deny access to the server
+  // Conditionally allow or deny access to the server 
   if (event.username === process.env.FTP_USERNAME && event.password === process.env.FTP_PASSWORD) {
     return {
       Role: process.env.ROLE_ARN,
       PosixProfile: {
-        Uid: parseInt(process.env.UID || '0'),
-        Gid: parseInt(process.env.GID || '0'),
+        Uid: parseInt(process.env.EFS_UID || '0'),
+        Gid: parseInt(process.env.EFS_GID || '0'),
       },
       Policy: '',
       HomeDirectory: `/${process.env.EFS_ID}`,
